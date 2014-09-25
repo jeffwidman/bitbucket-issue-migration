2f9d51f53f288eb4a04912ea6e248224a52829e4
emilio <feedingaliencat@gmail.com>
2014-09-25 17:39:12 +0200

    shebang and utf-8

--- migrate.py
+++ migrate.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
 # This file is part of the bitbucket issue migration script.
 #
 # The script is free software: you can redistribute it and/or modify
